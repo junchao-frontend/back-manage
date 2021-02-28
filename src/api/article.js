640b71f08725bbf8fea494d58b41605620b6e251
junchao <1109355524@qq.com>
2021-02-28 17:06:46 +0800

    2.28.17

---
+++ src/api/article.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+export const getArticles =params =>{
+    return request({
+        method: 'GET',
+        url:'/mp/v1_0/articles',
+        //Body 参数使用data 设置
+        //Query 参数使用params设置
+        //Headers 参数使用headers设置
+        params
+    })
+}
