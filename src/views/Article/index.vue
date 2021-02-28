671fcbc7e42847e3a0bb63ca25395b6daad38291
junchao <1109355524@qq.com>
2021-02-28 17:24:09 +0800

    222

--- src/views/Article/index.vue
+++ src/views/Article/index.vue
@@ -1,75 +1,71 @@
 <template>
   <div>
-      <el-card class="card1">
+    <el-card class="card1">
       <div slot="header" class="clearfix">
-       <!-- 面包屑 -->
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>内容管理</el-breadcrumb-item>
-      </el-breadcrumb>
-      <!-- 面包屑 -->
+        <!-- 面包屑 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 面包屑 -->
       </div>
-    <!-- form表单 -->
-    <el-form ref="form" :model="form" label-width="40px" size="mini">
-      <el-form-item label="状态">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="全部"></el-radio>
-          <el-radio label="草稿"></el-radio>
-          <el-radio label="待审核"></el-radio>
-          <el-radio label="审核通过"></el-radio>
-          <el-radio label="审核失败"></el-radio>
-          <el-radio label="已删除"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="频道">
-        <el-select v-model="form.region" placeholder="请选择频道">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="日期">
-        <el-date-picker
-          v-model="value1"
-          type="datetimerange"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-          :default-time="['12:00:00']"
-        >
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
-    <!-- form表单 -->
+      <!-- form表单 -->
+      <el-form ref="form" :model="form" label-width="40px" size="mini">
+        <el-form-item label="状态">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="全部"></el-radio>
+            <el-radio label="草稿"></el-radio>
+            <el-radio label="待审核"></el-radio>
+            <el-radio label="审核通过"></el-radio>
+            <el-radio label="审核失败"></el-radio>
+            <el-radio label="已删除"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择频道">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期">
+          <el-date-picker
+            v-model="value1"
+            type="datetimerange"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :default-time="['12:00:00']"
+          >
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- form表单 -->
     </el-card>
     <el-card class="card2">
-      <div slot="header" class="clearfix">
-      </div>
-       <!-- 列表数据 -->
-    <el-table :data="tableData"  stripe style="width: 100%" class="table1">
-      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-      <el-table-column prop="address" label="地址"> </el-table-column>
-    </el-table>
-    <!-- 列表数据 -->
-      <el-pagination
-      background
-      layout="prev, pager, next"
-      :total="1000">
+      <div slot="header" class="clearfix"></div>
+      <!-- 列表数据 -->
+      <el-table :data="tableData" stripe style="width: 100%" class="table1">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+      <!-- 列表数据 -->
+      <el-pagination background layout="prev, pager, next" :total="1000">
       </el-pagination>
-      </el-card>
-
-
+    </el-card>
   </div>
 </template>
 
 <script>
-import {getArticles} from '@/api/article'
+import { getArticles } from '@/api/article'
 export default {
   name: "ArticleIndex",
 
-  data() {
+  data () {
     return {
       form: {
         name: "",
@@ -81,47 +77,47 @@ export default {
         resource: "",
         desc: "",
       },
-      value1:'',
-       tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }],
-        articles:[]
+      value1: '',
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      articles: []
     };
   },
-  created(){
-      this.loadArticles()
+  created () {
+    this.loadArticles()
   },
   methods: {
-    loadArticles(){
-      getArticles().then(res =>{
-        this.articles=res.data.data.results
+    loadArticles () {
+      getArticles().then(res => {
+        this.articles = res.data.data.results
       })
     },
-    onSubmit(){
+    onSubmit () {
       console.log(this.onSubmit)
     }
   }
 };
 </script>
 <style>
-.card1{
+.card1 {
   margin-bottom: 30px;
 }
-.table1{
+.table1 {
   margin-bottom: 20px;
 }
 </style>
